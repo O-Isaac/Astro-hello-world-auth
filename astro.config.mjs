c409de0a74db76a9829d6bbd777a00eb1a9ff2e3
O-Isaac <soporte@proyectograndorder.es>
2024-09-27 14:57:48 +0200

    deps(tailwind): Add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,11 @@ import vercel from '@astrojs/vercel/serverless';
 
 import db from '@astrojs/db';
 
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
-  integrations: [db()],
+  integrations: [db(), tailwind()],
 });
